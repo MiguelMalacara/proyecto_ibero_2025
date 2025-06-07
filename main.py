b668d4e9585ecbe1085f43a54d1040c1bf91f32a
MiguelMalacara <jmiguel.malacarah@gmail.com>
2025-06-06 19:44:49 -0600

    funcion df a np

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import pandas as pd
 import numpy as np 
-print("Hola mundo")
+
+def numpy_df():
+    df = pd.DataFrame(np.random.randn(5, 3), columns=["A", "B", "C"])
+    
+    print("DataFrame")
+    print(df)
+    
+    arr = df.to_numpy()
+    
+    print("\nNumpy Array:")
+    print(arr)
+    
+numpy_df()
